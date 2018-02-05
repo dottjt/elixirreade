11f1939a8d66fbef0a8f071a8935224af06f1912
Julius Reade <julius@reactordigital.com.au>
2018-02-06 09:11:14 +1100

    initial commit

---
+++ test/portfolio_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule PortfolioWeb.ErrorViewTest do
+  use PortfolioWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(PortfolioWeb.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(PortfolioWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(PortfolioWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
